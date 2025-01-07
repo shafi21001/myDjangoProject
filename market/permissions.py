c96e6413b0486c17da539ed0cb93b032c604abad
Md. Shafi Mahmud <shafi.msm1999@gmail.com>
2025-01-07 23:45:14 +0600

    Delete market directory

--- market/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_staff and request.user.is_superuser
-    
-class IsStaff(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_staff
